4c49dbe37937d8db455aaa3cc2fa8b1b827a7aae
ivan.dimitrov <ivan.dimitrov@fit.fraunhofer.de>
2023-09-15 17:20:17 +0200

    Initial Commit

---
+++ Hach2023_new/Hach2023_newApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hach2023_newApp.swift
+//  Hach2023_new
+//
+//  Created by dimitrov on 15.09.23.
+//
+
+import SwiftUI
+
+@main
+struct Hach2023_newApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hach2023_newUITests/Hach2023_newUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Hach2023_newUITestsLaunchTests.swift
+//  Hach2023_newUITests
+//
+//  Created by dimitrov on 15.09.23.
+//
+
+import XCTest
+
+final class Hach2023_newUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
